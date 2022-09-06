c9492e3c76f4de36b608245726924d4833dce28d
Muhammad Aswan <muhaswan190897@gmail.com>
2022-09-06 20:58:33 +0800

    convert html to nextjs - member, transaction-member, and edit-profile page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,6 +9,11 @@ import '../styles/sign-up-photo.css';
 import '../styles/sign-up-photo-success.css';
 import '../styles/sign-up-success.css';
 import '../styles/404-not-found.css';
+import '../styles/sidebar.css';
+import '../styles/overview.css';
+import '../styles/transactions.css';
+import '../styles/transactions-detail.css';
+import '../styles/edit-profile.css';
 
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
